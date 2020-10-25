921620aa9b75d834179325b4c8d2deb2514c1ebf
jandresraba <jandresraba@gmail.com>
2020-10-25 00:07:55 -0500

    Iniciando el formulario

---
+++ src/main/java/com/mycompany/miprimerformulario/IndexController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.miprimerformulario;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Johan
+ */
+@Named(value = "indexController")
+@Dependent
+public class IndexController {
+
+    /**
+     * Creates a new instance of IndexController
+     */
+    public IndexController() {
+    }
+    
+}
